aaa44946874ec5f5af224caa89cd692523de8cdc
M@35464620@gmail.com <mansi.j4620@gmail.com>
2021-05-04 14:22:07 +0530

    modify + add files in candidate template and create views in candidate app.

--- candidates/views.py
+++ candidates/views.py
@@ -5,3 +5,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 class CandidateHomeView(LoginRequiredMixin, TemplateView):
     template_name = "candidates/home.html"
+
+class JobsView(TemplateView):
+    template_name = "candidates/avail_jobs.html"
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
